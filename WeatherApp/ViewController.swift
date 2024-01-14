7377bc1b02bd6647aaa18d0a329004d0f8e797f1
Zacharia Mwaura <75284078+ZachariaMwaura@users.noreply.github.com>
2024-01-14 17:40:42 -0500

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Zach Mwaura on 1/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
